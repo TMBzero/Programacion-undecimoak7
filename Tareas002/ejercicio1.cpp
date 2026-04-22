9ea4e04c4185e7af074504c3c934f92912eb2ae4
TMBzero <estebanjose1223@gmail.com>
2026-04-21 23:24:46 -0600

    Actualizando la tarea

--- Tareas002/ejercicio1.cpp
+++ Tareas002/ejercicio1.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 bool es_palindromo(string palabra) {
-    string palindromo = "";
-    int largo = palabra.length();
+    string palindromo = ""; // string para darle vuelta a la palabra
+    int largo = palabra.length(); // largo de la palabra
     
     for (int i = 0; i < largo; i++) {
-        palindromo += tolower(palabra[palabra.length() -1 - (i)]);
+        palindromo += tolower(palabra[palabra.length() -1 - (i)]); // se le concatena el ultimo caracter en minuscula
     }
     for (int i = 0; i < largo; i++) {
-        palabra[i] = tolower(palabra[i]);
+        palabra[i] = tolower(palabra[i]); // convertimos en minusculas toda la palabra
     }
     
     if (palindromo == palabra) {
-        return true;
+        return true; // si son iguales retorna true
     } else if (palindromo != palabra) {
-        return false;
+        return false; // si son diferentes retorna false
     }
 }
 
 int main() {
-    string palabra;
+    string palabra; // definimos la palabra
     cout << "Ingrese su palabra" << '\n';
-    cin >> palabra;
-    if (es_palindromo(palabra) == true) {
+    cin >> palabra; // se ingresa la palabra
+    if (es_palindromo(palabra) == true) { // si la funcion retorna true, se indica que es palindromo
         cout << "'<" << palabra << ">' es palindromo" << '\n';
     } else if (es_palindromo(palabra) == false) {
         cout << "'<" << palabra << ">' no es palindromo" << '\n';
--- Tareas002/ejercicio2.cpp
+++ Tareas002/ejercicio2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 bool es_valido(int a, int b, int c) {
-    if (a+b > c){
+    if (a+b > c){ 
         if (b+c > a) {
             if (c+a > b){
                 return true;
@@ -17,35 +17,35 @@ bool es_valido(int a, int b, int c) {
         }
     } else {
         return false;
-}
+} // se comprueban los 3 casos y que se cumpla en los tres, si en uno solo no se comple, retorna false
 }
 
 string tipo_triangulo(int a, int b, int c) {
-    if (a==b && a==c) {
+    if (a==b && a==c) { //si todos son iguales
         return "Equilatero";
-    } else if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b)) {
+    } else if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b)) { // si solo 2 son iguales, con los 3 casos
         return "Isosceles";
-    } else {
+    } else { // si ninguno se cumple, los 3 diferentes
         return "escaleno";
     }
 
 }
 
 int calcular_perimetro(int a, int b, int c) {
-    return a+b+c;
+    return a+b+c; //se suman para el perimetro
 
 }
 
 void describir_triangulo(int a, int b, int c) {
-    string Tipo;
-    int perimetro;
-    if (es_valido(a, b, c) == true) {
-        Tipo = tipo_triangulo(a, b, c);
-        perimetro = calcular_perimetro(a, b, c);
-        cout << "Tipo: " << Tipo << '\n';
-        cout << "Perimetro: " << perimetro << '\n';
+    string Tipo; // definimos variables
+    int perimetro; 
+    if (es_valido(a, b, c) == true) { // llamamos y validamos
+        Tipo = tipo_triangulo(a, b, c); // llamamos y definimos al tipo
+        perimetro = calcular_perimetro(a, b, c); // llamamos y definimos el perimetro
+        cout << "Tipo: " << Tipo << '\n'; // imprimimos
+        cout << "Perimetro: " << perimetro << '\n'; // imprimimos
     } else {
-        cout << "Triangulo invalido" << '\n';
+        cout << "Triangulo invalido" << '\n'; // si no es valido, lo indica y salta las llamadas
     }
     
 
@@ -55,8 +55,8 @@ int main() {
     int a, b, c;
     cin >> a;
     cin >> b;
-    cin >> c;
-    describir_triangulo(a, b, c);
+    cin >> c; // definimos e ingresamos 
+    describir_triangulo(a, b, c); // llamamos a la funcion 
 }
 
 
